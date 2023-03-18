c1cae8d0c14e47b81d1ec2491559e212687d2670
Pavel Shnurov <pavel.shnurov@gmail.com>
2023-03-18 02:47:48 +0100

    feat: add js animations

--- js/script.js
+++ js/script.js
@@ -31,3 +31,25 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
+
+/* reval */
+ScrollReveal({
+        reset: true,
+        distance: '80px',
+        duration: 2000,
+        delay: 200
+    });
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
+
+/* typed */
+const typed = new Typed('.multiple-text', {
+    strings: ['Netrunner', 'Cyberpunk', 'Frontend Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
+
